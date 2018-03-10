b21619660fc830e255dc6e5d60f0d4c4509ffcda
Don Eaves <Don_Eaves@symantec.com>
2018-03-10 10:50:46 -0800

    rolled back import change

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	//SYMANTEC ODIN CHANGE. Using relative import, to allow local clone
-	// and install of odin branch of this plugin:
-	// "github.com/terraform-providers/terraform-provider-aws/aws"
-	"terraform-provider-aws/aws"
+	"github.com/terraform-providers/terraform-provider-aws/aws"
 )
 
 func main() {
